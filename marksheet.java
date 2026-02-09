67b10875102abc43195e9810bbb7d53bcbb99a3f
RajeshSongara <rajsongara121@gmail.com>
2026-02-09 14:41:47 +0530

    update code

--- marksheet.java
+++ marksheet.java
@@ -4,11 +4,12 @@ import java.awt.event.*;
 public class marksheet {
 
     Frame f;
-    TextField t1, t2, t3, t4, t5, tTotal, tPer, tGrade;
-    Label l1, l2, l3, l4, l5, l6, l7, l8;
+    TextField t1, t2, t3, t4, tTotal, tPer, tGrade;
+    Label l1, l2, l3, l4, l6, l7, l8;
     Button btn;
 
     marksheet() {
+
         f = new Frame("Marksheet");
 
         // Labels
@@ -16,8 +17,7 @@ public class marksheet {
         l2 = new Label("Sub 2");
         l3 = new Label("Sub 3");
         l4 = new Label("Sub 4");
-        l5 = new Label("Marks");
-        l6 = new Label("Total");
+        l6 = new Label("Obtained");   // <-- total hata ke obtained
         l7 = new Label("Percentage");
         l8 = new Label("Grade");
 
@@ -26,7 +26,6 @@ public class marksheet {
         t2 = new TextField();
         t3 = new TextField();
         t4 = new TextField();
-        t5 = new TextField(); // marks
         tTotal = new TextField();
         tPer = new TextField();
         tGrade = new TextField();
@@ -34,52 +33,84 @@ public class marksheet {
         // Button
         btn = new Button("Show");
 
-        // setBounds(x, y, width, height)
-        l1.setBounds(50, 50, 60, 20);
-        t1.setBounds(120, 50, 80, 20);
+        // Set Bounds
+        l1.setBounds(50, 50, 80, 20);
+        t1.setBounds(150, 50, 80, 20);
 
-        l2.setBounds(50, 80, 60, 20);
-        t2.setBounds(120, 80, 80, 20);
+        l2.setBounds(50, 80, 80, 20);
+        t2.setBounds(150, 80, 80, 20);
 
-        l3.setBounds(50, 110, 60, 20);
-        t3.setBounds(120, 110, 80, 20);
+        l3.setBounds(50, 110, 80, 20);
+        t3.setBounds(150, 110, 80, 20);
 
-        l4.setBounds(50, 140, 60, 20);
-        t4.setBounds(120, 140, 80, 20);
+        l4.setBounds(50, 140, 80, 20);
+        t4.setBounds(150, 140, 80, 20);
 
-        l5.setBounds(50, 170, 60, 20);
-        t5.setBounds(120, 170, 80, 20);
+        btn.setBounds(150, 180, 80, 30);
 
-        btn.setBounds(100, 210, 80, 30);
+        l6.setBounds(50, 230, 80, 20);
+        tTotal.setBounds(150, 230, 80, 20);
 
-        l6.setBounds(50, 260, 60, 20);
-        tTotal.setBounds(120, 260, 80, 20);
+        l7.setBounds(50, 260, 80, 20);
+        tPer.setBounds(150, 260, 80, 20);
 
-        l7.setBounds(50, 290, 70, 20);
-        tPer.setBounds(120, 290, 80, 20);
+        l8.setBounds(50, 290, 80, 20);
+        tGrade.setBounds(150, 290, 80, 20);
 
-        l8.setBounds(50, 320, 60, 20);
-        tGrade.setBounds(120, 320, 80, 20);
-
-        // Add components
+        // Add Components
         f.add(l1); f.add(t1);
         f.add(l2); f.add(t2);
         f.add(l3); f.add(t3);
         f.add(l4); f.add(t4);
-        f.add(l5); f.add(t5);
         f.add(btn);
         f.add(l6); f.add(tTotal);
         f.add(l7); f.add(tPer);
         f.add(l8); f.add(tGrade);
 
-        // Window close
+        // Obtained marks field read-only
+        tTotal.setEditable(false);
+        tPer.setEditable(false);
+        tGrade.setEditable(false);
+
+        // Button Action
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                int a = Integer.parseInt(t1.getText());
+                int b = Integer.parseInt(t2.getText());
+                int c = Integer.parseInt(t3.getText());
+                int d = Integer.parseInt(t4.getText());
+
+                int obtained = a + b + c + d;
+                tTotal.setText(String.valueOf(obtained)); // <-- kitne number aaye
+
+                double percentage = (obtained / 400.0) * 100;
+                tPer.setText(String.format("%.2f", percentage));
+
+                String grade;
+                if (percentage >= 90)
+                    grade = "A";
+                else if (percentage >= 80)
+                    grade = "B";
+                else if (percentage >= 70)
+                    grade = "C";
+                else if (percentage >= 60)
+                    grade = "D";
+                else
+                    grade = "F";
+
+                tGrade.setText(grade);
+            }
+        });
+
+        // Window Close
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 f.dispose();
             }
         });
 
-        f.setSize(400, 400);
+        f.setSize(420, 400);
         f.setLayout(null);
         f.setVisible(true);
     }
